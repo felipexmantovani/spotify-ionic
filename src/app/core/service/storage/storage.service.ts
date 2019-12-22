020dc0d9e10ead88b732c47a509079a3ebe59aef
Felipe Mantovani <felipexmantovani@gmail.com>
2019-12-22 11:50:27 -0300

    Update storage.service.ts

--- src/app/core/service/storage/storage.service.ts
+++ src/app/core/service/storage/storage.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Injectable()
-export class StorageService {}
+export class StorageService {
+  constructor(private storage: Storage) {}
+
+  public setValue(key: string, value: string): void {
+    this.storage.set(key, value);
+  }
+
+  public getValue(key: string): Promise<string> {
+    return this.storage.get(key);
+  }
+
+  public clear(key: string): void {
+    this.storage.remove(key);
+  }
+}
