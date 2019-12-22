a6f116599bb9a6d7ad0ec8773015e4fd7ff36e15
Felipe Mantovani <felipexmantovani@gmail.com>
2019-12-22 11:51:46 -0300

    Implementado AutenticacaoService na tela de login

--- src/app/module/autenticacao/model/login.ts
+++ src/app/module/autenticacao/model/login.ts
@@ -1,12 +1,12 @@
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 export class Login {
-  login: string = null;
+  autenticacao: string = null;
   senha: string = null;
 
   public criarForm(formBuilder: FormBuilder): FormGroup {
     return formBuilder.group({
-      login: [this.login, [Validators.required, Validators.minLength(3)]],
+      autenticacao: [this.autenticacao, [Validators.required, Validators.minLength(3)]],
       senha: [this.senha, [Validators.required, Validators.minLength(3)]]
     });
   }
--- src/app/module/autenticacao/page/login/login.page.ts
+++ src/app/module/autenticacao/page/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Login } from '../../model/login';
+import { AutenticacaoService } from '../../service/autenticacao.service';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,10 @@ export class LoginPage implements OnInit {
   private login: Login = new Login();
   public loginForm: FormGroup = null;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private autenticacaoService: AutenticacaoService
+  ) {}
 
   ngOnInit() {
     this.criarForm();
@@ -22,7 +26,10 @@ export class LoginPage implements OnInit {
     this.loginForm = this.login.criarForm(this.formBuilder);
   }
 
-  public onSubmit(): void {}
+  public onSubmit(): void {
+    this.login = this.loginForm.value;
+    this.autenticacaoService.login(this.login);
+  }
 
-  public goEsqueciSenha(): void {}
+  public goEsqueciSenha() {}
 }
