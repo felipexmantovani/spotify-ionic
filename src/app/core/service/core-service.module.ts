57bcdc35431408dc3b4df6dc653726a4280aa0c3
Felipe Mantovani <felipexmantovani@gmail.com>
2019-12-22 11:50:13 -0300

    Instalado e configurado IonicStorageModule

--- src/app/core/service/core-service.module.ts
+++ src/app/core/service/core-service.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
+import { IonicStorageModule } from '@ionic/storage';
 import { StorageService } from './storage/storage.service';
 
 @NgModule({
+  imports: [IonicStorageModule.forRoot({
+    name: 'spotify-ionic-db',
+    driverOrder: ['indexeddb', 'sqlite', 'websql']
+  })],
   providers: [StorageService]
 })
 export class CoreServiceModule {}
