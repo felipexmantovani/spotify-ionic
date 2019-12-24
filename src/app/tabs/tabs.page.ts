ac04b4d8df4d9fbb56cea9352170777f900b2f1d
Felipe Mantovani <felipexmantovani@gmail.com>
2019-12-24 19:33:38 -0300

    Configurado links Tabs

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BibliotecaUtil } from '../module/biblioteca/util/biblioteca-util';
+import { UsuarioUtil } from '../module/usuario/util/usuario-util';
+import { TabsUtil } from './tabs-util';
 
 @Component({
   selector: 'app-tabs',
@@ -6,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
+  public pathTabsFront: string = TabsUtil.SETUP.path.front;
+  public pathUsuarioFront: string = UsuarioUtil.SETUP.path.front;
+  public pathBibliotecaFront: string = BibliotecaUtil.SETUP.path.front;
+
+  public pathInicio: string = `${this.pathTabsFront}${this.pathUsuarioFront}/inicio`;
+  public pathBuscar: string = `${this.pathTabsFront}${this.pathBibliotecaFront}/buscar`;
+  public pathBiblioteca: string = `${this.pathTabsFront}${this.pathBibliotecaFront}/inicio`;
+
   constructor() {}
 }
