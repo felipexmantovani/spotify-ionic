c2646f5102bdf3f1fbd4cde8f737e1e42a39a2db
Felipe Mantovani <felipexmantovani@gmail.com>
2019-12-23 22:34:02 -0300

    Update autenticacao.service.ts

--- src/app/module/autenticacao/service/autenticacao.service.ts
+++ src/app/module/autenticacao/service/autenticacao.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
+import { NavController } from '@ionic/angular';
 import { StorageService } from '../../../core/service/storage/storage.service';
+import { SistemaUtil } from '../../sistema/util/sistema-util';
+import { UsuarioUtil } from '../../usuario/util/usuario-util';
 import { Login } from '../model/login';
 import { AutenticacaoUtil } from '../util/autenticacao-util';
 
@@ -7,17 +10,21 @@ import { AutenticacaoUtil } from '../util/autenticacao-util';
   providedIn: 'root'
 })
 export class AutenticacaoService {
-  constructor(private storageService: StorageService) {}
+  constructor(private storageService: StorageService, private navController: NavController) {}
 
   public login(login: Login): void {
     this.storageService.setKey(
       AutenticacaoUtil.SETUP.storageKey.token,
-      `(tokenFake)_${login.autenticacao}_${Math.random().toString(36).slice(-`${login.senha.length}`)}`
+      `(tokenFake)_${login.autenticacao}_${Math.random()
+        .toString(36)
+        .slice(-`${login.senha.length}`)}`
     );
+    this.navController.navigateRoot(UsuarioUtil.SETUP.path.front);
   }
 
   public logout(): void {
     this.storageService.removeKey(AutenticacaoUtil.SETUP.storageKey.token);
+    this.navController.navigateRoot(SistemaUtil.SETUP.path.front);
   }
 
   public async isLogado(): Promise<boolean> {
